11e5a930928c1a57256009c0ecc8928e0800c8db
JoseLuisCas <A01707764@itesm.mx>
2021-03-18 19:17:15 -0600

    Juego terminado de snake

--- snake.py
+++ snake.py
@@ -9,27 +9,35 @@ Exercises
 
 """
 
-from turtle import *
-from random import randrange
+from turtle import update, clear, ontimer, \
+    hideturtle, tracer, listen, onkey, done
+from random import randrange, randint
 from freegames import square, vector
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
+
 def change(x, y):
     "Change snake direction."
     aim.x = x
     aim.y = y
 
+
 def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
 
+
 def move():
-    "Move snake forward one segment."
+    " Move snake forward one segment. "
     head = snake[-1].copy()
     head.move(aim)
+    pocision = 0
+    colores = ['purple', 'blue', 'green', 'pink',
+               'brown', 'yellow', 'black', 'orange']
+    pocision = randint(0, 7)
 
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
@@ -42,19 +50,20 @@ def move():
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
+
     else:
         snake.pop(0)
 
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
-
+        col = colores[pocision]
+        square(body.x, body.y, 9, col)
     square(food.x, food.y, 9, 'green')
     update()
     ontimer(move, 100)
 
-setup(420, 420, 370, 0)
+
 hideturtle()
 tracer(False)
 listen()
